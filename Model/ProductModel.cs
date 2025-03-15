66cd48c84d3c6831eb30a96c115b93c8fc61bdb8
Eltemoriste0713 <cuauhtemoc.araiza.21isc@tecsanpedro.edu.mx>
2025-03-14 21:23:39 -0600

    Creación del modelo de datos de la Tabla de Production.ProductModel, creación del AppDbContext y modificación del Json

---
+++ Model/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml;
+
+namespace AdventureWork_CAAC.Model
+{
+    public class ProductModel
+    {
+        [Key]
+        public required int ProductModelID { get; set; }
+        public required string Name { get; set; }
+        public string CatalogDescription { get; set; } = string.Empty;
+        public string Instructions { get; set; } = string.Empty;
+        public required Guid rowguid { get; set; }
+        public required DateTime ModifiedDate { get; set; }
+
+    }
+}
